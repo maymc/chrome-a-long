cef9c1e0356efed2e1e0d1b796ac518e45ce85ec
Sarah <yamashige.sarah@gmail.com>
2018-08-11 12:02:43 -1000

    Got all the arrays into one master array

--- content.js
+++ content.js
@@ -1,24 +1,23 @@
 console.log("Sanity check");
 
+//Our array of arrays
 let wordArr = [];
 
 let pElems = document.getElementsByTagName('p');
 console.log("someting?")
-for (let i = 0; i < pElems.length; i++) {
+for (let i = 0; i < pElems.length; i++) {           // Loops through all of the "p"  elems
     let elem = pElems[i];
     // console.log("working?")
     console.log("P Elem", elem)
     wordArr.push((elem.innerText).split(' ')) 
      
 }
-console.log(wordArr);
+console.log(wordArr);                               // Logs an array of arrays of words from each pElem[i]
 
-let otherArr = [];
-wordArr[2].forEach(elem => {
-    
-    otherArr.push((elem.split(',')).join(" "));
-})
+//Our master array of every word 
+let masterArr =[];
 
-let tryingThis = otherArr.push(wordArr[2].join(" "));
-console.log("other Array", tryingThis)
-console.log(wordArr[2])
+for (let j = 0; j < wordArr.length; j++) {
+    masterArr = masterArr.concat(wordArr[j]);       // Joining all of the arrays into one master array
+}
+console.log("Master", masterArr);
