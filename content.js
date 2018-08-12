43ebc79c62e731fb83e5e34a72d36471b4d3b203
May Chen <maychen@Mays-MacBook-Pro.local>
2018-08-11 14:27:01 -1000

    didn't make new edits

--- content.js
+++ content.js
@@ -5,33 +5,20 @@ let wordArr = [];
 
 let pElems = document.getElementsByTagName("p");
 console.log("someting?");
+
 for (let i = 0; i < pElems.length; i++) {
+  // Loops through all of the "p"  elems
   let elem = pElems[i];
   // console.log("working?")
   console.log("P Elem", elem);
   wordArr.push(elem.innerText.split(" "));
-  let pElems = document.getElementsByTagName("p");
-  console.log("someting?");
+}
+console.log(wordArr); // Logs an array of arrays of words from each pElem[i]
 
-  for (let i = 0; i < pElems.length; i++) {
-    // Loops through all of the "p"  elems
-    let elem = pElems[i];
-    // console.log("working?")
-    console.log("P Elem", elem);
-    wordArr.push(elem.innerText.split(" "));
-  }
-  console.log(wordArr); // Logs an array of arrays of words from each pElem[i]
+//Our master array of every word
+let masterArr = [];
 
-  let otherArr = [];
-  wordArr[2].forEach(elem => {
-    otherArr.push(elem.split(",").join(" "));
-  });
-  console.log("other Array", otherArr);
-  //Our master array of every word
-  let masterArr = [];
-
-  for (let j = 0; j < wordArr.length; j++) {
-    masterArr = masterArr.concat(wordArr[j]); // Joining all of the arrays into one master array
-  }
+for (let j = 0; j < wordArr.length; j++) {
+  masterArr = masterArr.concat(wordArr[j]); // Joining all of the arrays into one master array
 }
 console.log("Master", masterArr);
